b0ae9a9c55468fc4f518f11869f328066b0a32c8
Terrance Li <terranceli.terbear@gmail.com>
2018-06-30 15:43:29 -0700

    test mongodb connection

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://admin:' + process.env.DB_PASSWORD + '@recordbookcluster0-shard-00-00-l24me.mongodb.net:27017,recordbookcluster0-shard-00-01-l24me.mongodb.net:27017,recordbookcluster0-shard-00-02-l24me.mongodb.net:27017/bonnie?ssl=true&replicaSet=RecordBookCluster0-shard-0&authSource=admin', {
+  useMongoClient: true
+});
+var db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to MongoDB!');
+});
+
+
 app.get('/', (req, res) => {
   res.send('pong!');
 });
