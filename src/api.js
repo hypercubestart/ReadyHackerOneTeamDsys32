ee4a936cec4f2e9a83072ca1ed78f736fceb8eb3
Terrance Li <terranceli.terbear@gmail.com>
2018-07-01 02:52:39 -0700

    populated api js

--- src/api.js
+++ src/api.js
@@ -42,5 +42,108 @@ var login = (email, password, callback) => {
   });
 }
 
+var logout = (callback) => {
+  const url = BASE_URL + '/user/logout';
 
-export { getItems, register, login };
+  axios.post(url, {}, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var placeOrder = (items, callback) => {
+  const url = BASE_URL + '/order/place';
+
+  axios.post(url, {items: items}, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var isAuthenticated = (callback) => {
+  const url = BASE_URL + '/user/authenticated';
+
+  axios.get(url, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var getUser = (callback) => {
+  const url = BASE_URL + '/user/get';
+
+  axios.get(url, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var fulfillOrder = (id, callback) => {
+  const url = BASE_URL + '/order/fulfill';
+
+  axios.post(url, {id: id}, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+var cancelOrder = (id, callback) => {
+  const url = BASE_URL + '/order/cancel';
+
+  axios.post(url, {id: id}, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+var getOrders = (callback) => {
+  const url = BASE_URL + '/order/get';
+
+  axios.get(url, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+var exportOrders = (callback) => {
+  const url = BASE_URL + '/order/export';
+
+  axios.get(url, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+var registerStaff = (name, email, password, callback) => {
+  const url = BASE_URL + '/staff/register';
+
+  axios.post(url, {
+    name: name,
+    email: email,
+    password: password
+  }, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var loginStaff = (email, password, callback) => {
+  const url = BASE_URL + '/staff/login';
+
+  axios.post(url, { email: email, password: password }, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+var logoutStaff = (callback) => {
+  const url = BASE_URL + '/staff/logout';
+
+  axios.post(url, {}, {withCredentials: true}).then((res) => {
+    callback(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+var addItem = (name, description, price, category, picture, callback) => {
+
+}
+
+var removeItem = (id, callback) => {
+  const url = BASE_URL + '/item/remove';
+
+  axios.post(url, {id: id}, {withCredentials: true}).then((res) => callback(res)).catch((err) => console.log(err));
+}
+
+export { getItems, register, login, logout, placeOrder, isAuthenticated, getUser, fulfillOrder, cancelOrder, getOrders, exportOrders, registerStaff, loginStaff, logoutStaff, addItem, removeItem };
