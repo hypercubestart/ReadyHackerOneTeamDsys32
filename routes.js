db1bba552c95bef2e0c624fcca49c939a55ba09e
Terrance Li <terranceli.terbear@gmail.com>
2018-06-30 18:42:03 -0700

    check auth route done

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ module.exports = (app) => {
   app.route('/user/login/').post(bonnieMiddleware.authenticate).post(bonnieController.login);
   app.route('/user/register/').post(bonnieController.register);
   app.route('/user/logout/').post(bonnieMiddleware.verifySession).post(bonnieController.logout);
+  app.route('/user/authenticated/').post(bonnieMiddleware.verifySession).post((req, res) => res.status(200).end());
 
   
   app.route('/items/get/').get(bonnieMiddleware.verifySession).get(bonnieController.fetchItems);
