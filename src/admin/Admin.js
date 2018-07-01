2d123ebaabfb9bacc9141ff9a2a20fb5616e3ebb
hypercubestart <andrewlliu@gmail.com>
2018-07-01 03:46:06 -0700

    starting admin page

---
+++ src/admin/Admin.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import io from 'socket.io-client'
+
+export default class Admin extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            currentOrders: []
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return(
+
+        )
+    }
+}
