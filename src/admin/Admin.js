91d879b1b27d79ce0fb05e569dbaf59cae657aa2
Nathan Yan <s-yann@bsd405.org>
2018-07-01 07:49:12 -0700

    Added staff page and button

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -3,7 +3,8 @@ import {OrderStep} from "../order/Order";
 import React, { Component } from 'react'
 import io from "socket.io";
 
-import { getOrders, getItems } from '../api';
+import { getOrders, getItems, getStaff } from '../api';
+import Button from "../components/Button";
 import moment from 'moment';
 
 export default class Admin extends Component {
@@ -43,6 +44,26 @@ export default class Admin extends Component {
     } 
 
     render() {
+        var page;
+        if (this.state.tab == 0){
+            page = <div>
+                <div style = {{width: "100%", display: "flex"}}>
+                    <div style = {{width: "50%"}}>
+                        {this.state.currentOrders.map((order) => {
+                            return <Order data = {order} availableItems={this.state.items}></Order>
+                        })}
+                    </div>
+                    <div style = {{width: "50%"}} orders = {this.state.currentOrders}></div>
+                </div>
+            </div>;
+        }else if (this.state.tab == 2){
+            page = <div>
+                <div style = {{width: "100%"}}>
+                    <StaffPage></StaffPage>
+                </div>
+            </div>
+        }
+
         return(
             <div>
                 <div style = {{position: "fixed", top: "0", left: "0", width: "100%", height: "200px", background: "white", zIndex: "10"}}></div>
@@ -53,16 +74,8 @@ export default class Admin extends Component {
                         <AdminLink content = "staff" selected = {this.state.tab == 2} style = {{textAlign: "left"}} callback = {() => this.changeTab(2)}></AdminLink>
                     </div>
 
-                    <div>
-                        <div style = {{width: "100%", display: "flex"}}>
-                            <div style = {{width: "50%"}}>
-                                {this.state.currentOrders.map((order) => {
-                                    return <Order data = {order} availableItems={this.state.items}></Order>
-                                })}
-                            </div>
-                            <div style = {{width: "50%"}} orders = {this.state.currentOrders}></div>
-                        </div>
-                    </div>
+                    {page}
+
                 </div>
             </div>
         )
@@ -86,6 +99,75 @@ class AdminLink extends Component {
     }
   }
 
+class StaffPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            staff: [],
+            addingMember: false
+        }
+    
+        this.showAddMember = this.showAddMember.bind(this);
+    }
+    
+    showAddMember(){
+        this.setState({
+            addingMember: true
+        })
+    }
+
+    hideAddMember() {
+        this.setState({
+            addingMember: false
+        })
+    }
+
+    componentWillMount(){
+        getStaff((res) => {
+            console.log(res.data);
+
+            let staff = res.data;
+            this.setState({
+                staff: staff
+            });
+        });
+    }
+
+
+
+    render() {
+        return <div style = {{}}>
+            {this.state.staff.map((member) => {
+                return <div style = {{width: "45%", marginLeft: "2.5%", height: "140px", border: "3px #1c5bff solid", borderRadius: "15px", padding: "15px"}}>
+                    <Staff data = {member}></Staff>
+                    <Button content = 'new staff' style = {{width: "fit-content", color : "white", background : "#1c5bff", position: "fixed", bottom: "50px", right: "100px"}} onClick = {this.showAddMember}></Button>
+
+                    {this.state.addingMember && <StaffCreateForm></StaffCreateForm>}
+                </div>
+            })}
+           
+        </div>
+    }
+}
+
+class Staff extends Component {
+    render() {
+        return <div>
+            <div style = {{fontSize: "35px"}}>{this.props.data.name}</div>
+            <div style = {{fontSize: "25px", color: "#ccc"}}>{this.props.data.email}</div>
+        </div>
+    }
+}
+
+class StaffCreateForm extends Component {
+    render () {
+        return <div className = "z-depth-3" style = {{position: "fixed", top: "15%", left: "15%", width: "70%", height: "70%", background: "white", padding: "30px"}}>
+            <div style = {{width: "100%", textAlign: "center", color: "#1c5bff", fontSize: "50px"}}>new staff</div>
+        </div> 
+    }
+}
+
   class Order extends Component {
     
     render () {
@@ -95,16 +177,17 @@ class AdminLink extends Component {
         var borderString = '3px solid #527aff';
 
         var itemNames = [];
+        console.log(this.props.availableItems);
 
         for (var i = 0; i < this.props.data.items.length; i++){
           var item = this.props.data.items[i];
           
           for (var j = 0; j < this.props.availableItems.length; j++) {
-            if (item._id == this.props.availableItems[i]._id) {
+            if (item._id == this.props.availableItems[j]._id) {
               if (item.quantity > 1) 
-                itemNames.push(this.props.availableItems[i].name + ' (x' + item.quantity + ')');
+                itemNames.push(this.props.availableItems[j].name + ' (x' + item.quantity + ')');
               else if (item.quantity == 1)
-                itemNames.push(this.props.availableItems[i].name);
+                itemNames.push(this.props.availableItems[j].name);
                 
               break;
             }
